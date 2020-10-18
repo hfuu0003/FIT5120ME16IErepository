3cc164c62c1d4ecd860fd09e2fc4475a99d2c170
Herry <hfuu0003@student.monash.edu>
2020-10-18 16:26:25 +0800

    Modify the map display style

--- scripts/SelectArea.php
+++ scripts/SelectArea.php
@@ -1,7 +1,7 @@
 <form method="post">
     <div style="text-align: center;vertical-align: middle;"><br>
         <label><?php _e('Select an area here: '); ?></label>
-        <input type="text" name="area" list="areas"/>
+        <input type="text" required="required" name="area" list="areas"/>
         <datalist id="areas" style="display: none; max-height:150px; overflow:auto;">
             <?php
             global $wpdb;
@@ -12,7 +12,7 @@
             }
             ?>
         </datalist>
-        <button type="submit" name="submit" class="submit button"><?php _e('Check now'); ?></button><br>
+        <button type="submit" name="submit" class="submit button"><?php _e('Check now'); ?></button><br><br>
     </div>
 </form>
 
@@ -30,16 +30,16 @@ if (isset($_POST['submit'])) {
         $count = $row->Count;
     }
     if ($exist){
-        echo' <br><h2> ' .$area. ' contains <font color="red">'.$count.' toxic plants</font> to dogs !!</h2><br>';
+        echo'<div style="text-align: center;vertical-align: middle; background-color: #FF9900; font-size: 28px;">' .$area. ' contains '.$count.' toxic plants to dogs !!</div>';
         echo '<div class="holds-the-iframe">
 
                     <iframe style="width: 100%; height: 500px;" defer src="'.$link.'" allowfullscreen="alloallowfullscreen"></iframe>
 
-               </div>"';
+               </div>';
 
     }
     else{
-        echo'<p><img src="https://www.myhealthypets.tk/wp-content/uploads/2020/09/jingtanhao.jpg" width="80" height="80" /><code>'.$area. '</code> is not in Victoria or it don\'t contain any toxic plants to dog! <br> Please select one from the select box! </p>';
+        echo'<p align="center"><img src="https://www.myhealthypets.tk/wp-content/uploads/2020/09/jingtanhao.jpg" width="80" height="80" /><code>'.$area. '</code> is not in Victoria or it don\'t contain any toxic plants to dog! <br> Please select one from the select box! </p>';
     }
 
 }
